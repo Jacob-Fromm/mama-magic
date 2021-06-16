ca65ab1c00353cd55c3329dcc64af46541631944
jakeFromm <jacobfromm@gmail.com>
2021-06-16 16:08:39 -0400

    attempting to display logo on desktop build

--- components/Hero.js
+++ components/Hero.js
@@ -3,13 +3,16 @@ import Image from 'next/image'
 
 export default function Logo() {
     return(
-        <div className="flex ml-20 justify-end items-center content-center">
-            <div className='flex-1 ml-10 my-10'>
-                <h2 className="text-amethyst-smoke-DEFAULT">We want mamas to feel inspired by their own strength and connected to all the other mamas of the world. Early motherhood brings joyful moments, but also physical and emotional challenges. Through it all, we want to remind every mama just how full of magic she is.
+        <div className="grid grid-cols-2 mx-20 justify-end items-center content-center">
+            <div className="flex flex-col ml-24">
+            <div className='flex-1 mb-5'>
+                <h2 className="text-amethyst-smoke-DEFAULT font-bold">We want mamas to feel inspired by their own strength and connected to all the other mamas of the world. Early motherhood brings joyful moments, but also physical and emotional challenges. Through it all, we want to remind every mama just how full of magic she is.
                 </h2>
-            <div className="flex-1 my-5"></div>
-                <h2 className="text-amethyst-smoke-DEFAULT">May you courageously channel the mamas who came before you and all the other mamas who are doing this <em>right now</em>. Your experience is unique to you, but you’re not doing this alone. You’ve got something special in you that binds us all. It’s mama magic.
+            </div>
+            <div className="flex-1">
+                <h2 className="text-amethyst-smoke-DEFAULT font-bold">May you courageously channel the mamas who came before you and all the other mamas who are doing this <em>right now</em>. Your experience is unique to you, but you’re not doing this alone. You’ve got something special in you that binds us all. It’s mama magic.
                 </h2>
+            </div>
             </div>
             <div className="flex-1 mx-0">
                 <Image 
